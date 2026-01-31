ac1fc4a514c664e1577d6fcb1acb6a54aaa77293
Prince <princeshrestha2111018@gmail.com>
2026-02-01 00:05:50 +0545

    feat: restructure authentication and user management - Added new configuration and database connection files. - Implemented user registration and login functionality with email verification. - Integrated email sending functionality using Resend API.

---
+++ backend/src/routes/authRoute.js
@@ -0,0 +1,24 @@
+import express from "express";
+import authController from "../controllers/authController.js";
+
+const router = express.Router();
+
+// URL: /api/auth/register
+router.post("/register", authController.registerUser);
+
+//URl: /api/auth/login
+router.post("/login", authController.loginUser);
+
+// URL: /api/auth/forgot-password
+router.post("/forgot-password", authController.forgotPassword);
+
+// URL: /api/auth/reset-password
+router.post("/reset-password", authController.resetPassword);
+
+// URL: /api/auth/verify-email/:userId/:token
+router.post("/verify-email", authController.verifyAccount);
+
+// URL: /api/auth/resend-verification
+router.post("/resend-verification", authController.resendVerification);
+
+export default router;
